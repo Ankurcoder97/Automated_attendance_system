dc516da5ec5009136c983e210cb6fe3390d9caf7
Ankurcoder97 <ankur2020banerjee@gmail.com>
2026-04-28 20:00:56 +0530

    "message"

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -1,140 +1,114 @@
 import streamlit as st
 
+
+
 def style_background_home():
-    st.markdown(
-        """
+
+    st.markdown("""
         <style>
-            .stApp{
-                background: #5856F2 !important; /* home */
-            }
-            
-            .stApp div[data-testid="stColumn"]{
-                background-color : #E0E3FF !important;
-                padding: 2.5rem !important;
-                border-radius: 5rem !important;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+
+                .stApp {
+                    background: #5865F2 !important;
+                }
+
+                .stApp div[data-testid="stColumn"]{
+                    background-color:#E0E3FF !important;
+                    padding:2.5rem !important;
+                    border-radius: 5rem !important;
+                    }
+        </style>  
+
+                """
+            ,unsafe_allow_html=True)
+    
 
 def style_background_dashboard():
-    st.markdown(
-        """
+
+    st.markdown("""
         <style>
-            .stApp{
-                background: #E0E3FF !important; /* dashboard */
-            }
-            
-            
-            /* Text input + password input box */
-            .stTextInput input {
-                background-color: white !important;
-                color: black !important;
-                border: 1px solid #ddd !important;
-                # border-radius: 12px !important;
-            }
 
-            /* Placeholder text */
-            .stTextInput input::placeholder {
-                color: #777 !important;
-            }
+                .stApp {
+                    background: #E0E3FF !important;
+                }
 
-            /* Focus effect */
-            .stTextInput input:focus {
-                border: 2px solid #5865F2 !important;
-                box-shadow: 0 0 0 2px rgba(88,101,242,0.15) !important;
-            }
+        </style>  
 
-            /* Password eye icon area */
-            [data-testid="stTextInput"] {
-                background: transparent !important;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+                """
+            ,unsafe_allow_html=True)
+    
+
+    
 
 def style_base_layout():
-    st.markdown(
-        """
+# asdasd
+    st.markdown("""
         <style>
-        @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis&family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap');
-        
-        
-            # /*Hide top bar of streamlit */
-            # #mainMenu , footer, header{
-            #     visibility: hidden;
-            # }
-            
-            # .block-container{
-            #     padding-top: 1.5rem !important;
-            # }
-            
-            h1{
+        @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
+
+                
+         /* Hide Top Bar of streamlit */
+                
+            #MainMenu, footer, header {
+                visibility: hidden;
+            }
+                
+            .block-container {
+                padding-top:1.5rem !important;    
+            }
+
+            h1 {
                 font-family: 'Climate Crisis', sans-serif !important;
                 font-size: 3.5rem !important;
-                line-height: 0.9 !important;
-                margin-bottom: 0rem !important;
-                # color: #000 !important;
+                line-height:1.1 1important;
+                margin-bottom:0rem !important;
             }
-            h2{
+                
+
+            h2 {
                 font-family: 'Climate Crisis', sans-serif !important;
                 font-size: 2rem !important;
                 line-height: 0.9 !important;
                 margin-bottom: 0rem !important;
-                color: #000 !important;
-                letter-spacing: 2px !important;
+                color: black !important;
             }
+                
+            h3, h4, p {
+                font-family: 'Outfit', sans-serif;    
 
-            .head h2{
-                color:#5865F2 !important;
             }
-            
-            
-            h3, h4, p{
-                font-family: 'Noto Serif', serif !important;
-                # font-optical-sizing: auto !important;
-                # font-weight: 400 !important;
-                # font-style: normal !important;
-                color: #000 !important;
-            }
-        
-            .stButton > button{
+                
+
+            button{
                 border-radius: 1.5rem !important;
-                background: #5865F2 !important;
+                background-color: #5865F2 !important;
                 color: white !important;
                 padding: 10px 20px !important;
                 border: none !important;
                 transition: transform 0.25s ease-in-out !important;
-            }
+                }
 
-            .stButton > button * {
-                color: white !important;
-            }
-
-            .stButton > button[kind="secondary"] {
+            button[kind="secondary"]{
                 border-radius: 1.5rem !important;
-                background: #EB459E !important;
+                background-color: #EB459E !important;
                 color: white !important;
                 padding: 10px 20px !important;
                 border: none !important;
                 transition: transform 0.25s ease-in-out !important;
-            }
+                }
 
-            .stButton > button[kind="tertiary"]{
+            button[kind="tertiary"]{
                 border-radius: 1.5rem !important;
-                background: black !important;
+                background-color: black !important;
                 color: white !important;
                 padding: 10px 20px !important;
                 border: none !important;
                 transition: transform 0.25s ease-in-out !important;
-            }
-            
-            .stButton > button:hover{
-                transform:scale(1.05)
-            }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+                }
+
+            button:hover{
+                transform :scale(1.05)}
+        </style>  
+
+                """
+            ,unsafe_allow_html=True)
