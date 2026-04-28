d159c6c5f1d074a25ab568ee3ff6543131b2b907
Ankurcoder97 <ankur2020banerjee@gmail.com>
2026-04-28 11:44:22 +0530

    "config"

--- src/database/config.py
+++ src/database/config.py
@@ -1,9 +1,13 @@
+# import streamlit as st
+
+
+# from supabase import create_client, Client
+
+# supabase: Client = create_client(
+#     st.secrets["SUPABASE_URL"],
+#     st.secrets["SUPABASE_KEY"]
+# )
 import streamlit as st
 
-
-from supabase import create_client, Client
-
-supabase: Client = create_client(
-    st.secrets["SUPABASE_URL"],
-    st.secrets["SUPABASE_KEY"]
-)
+st.write("Secrets loaded successfully")
+st.write(st.secrets)
