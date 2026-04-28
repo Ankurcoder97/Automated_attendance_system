e69c7838269d7caa0180da891806445232088d96
Ankurcoder97 <ankur2020banerjee@gmail.com>
2026-04-28 12:04:16 +0530

    "git"

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -77,7 +77,7 @@ def style_base_layout():
             }
                 
 
-            button [kind="primary"]{
+            button{
                 border-radius: 1.5rem !important;
                 background-color: #5865F2 !important;
                 color: white !important;
